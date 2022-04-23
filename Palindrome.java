8ed3f959df2a34f2f2a05ed030a172fecb19dd4d
Sreelekshmi91 <102054755+Sreelekshmi91@users.noreply.github.com>
2022-04-23 17:19:04 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,29 @@
+package week2.day1;
+
+public class Palindrome {
+
+public static void main(String[] args) {
+		
+		// To find the given string is palindrome or not
+
+	    String str = "madam";
+	    String revStr = "";
+	    
+	    int strLen = str.length();
+
+	    for (int i = (strLen - 1); i >=0; --i) {
+	      revStr = revStr + str.charAt(i);
+	    }
+
+	    if (str.toLowerCase().equals(revStr.toLowerCase())) {
+	      System.out.println(str + " is a Palindrome String.");
+	    }
+	    else {
+	      System.out.println(str + " is not a Palindrome String.");
+	    }
+	  }
+	}
+
+
+	
+
